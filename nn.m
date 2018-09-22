a7b3a7bd161f4401f9326f6fe817752dd2c03a2e
Imad Toubal <imad@xerise.com>
2018-09-22 15:30:55 -0500

    Works without beta

--- nn.m
+++ nn.m
@@ -11,33 +11,61 @@ input_count = size(W1,2) - 1;
 hidden_count = size(W1,1);
 output_count = size(W2, 1);
 
+epsilon = 0.001;
+alpha = 0.7;
+beta = 0.3;
+
 d = T(:,(input_count+1):end);
 
-for i = 1:size(T(:,1), 1)
-    currentX = [1, T(i,1:input_count)];
+epoch = 1;
+while(true)
+%         PREV_W1 = W1;
+%         PREV_W2 = W2;
     
-    % Hidden Layer
-    V1 = currentX * W1';
-    Y1 = [1 arrayfun(@sigmoid,V1)];
-    
-    % Output layer
-    V2 = Y1 * W2';
-    Y2 = arrayfun(@sigmoid,V2);
-    
-    %getting the error
-    E(i,:) = d(i,:) - Y2;
-    e=E(i,:);
-    
-    %Backpropagation
-    delta_out = e.*Y2.*(ones(1, output_count)-Y2);
-    
-    phi_prime = Y1.*(ones(1, hidden_count+1,1)-Y1);
-    delta_hidden = phi_prime.*(sum(W2.*delta_out'));
-    
-    %Updating Weights
-    change1 = delta_hidden' * currentX;
-    W1 = W1+change1(2:end,:);
-    
-    change2 = delta_out'*Y1;
-    W2 = W2+change2;
-end;
+    for i = 1:size(T(:,1), 1)
+        currentX = [1, T(i,1:input_count)];
+        
+        % Hidden Layer
+        V1 = currentX * W1';
+        Y1 = [1 arrayfun(@sigmoid,V1)];
+        
+        % Output layer
+        V2 = Y1 * W2';
+        Y2 = arrayfun(@sigmoid,V2);
+        
+        %getting the error
+        E(i,:) = d(i,:) - Y2;
+        e=E(i,:);
+        
+        %Backpropagation
+        delta_out = e.*Y2.*(ones(1, output_count)-Y2);
+        
+        phi_prime = Y1.*(ones(1, hidden_count+1,1)-Y1);
+        delta_hidden = phi_prime.*(sum(W2.*delta_out'));
+        
+        %Updating Weights
+        change1 = alpha * delta_hidden' * currentX;
+        change2 = alpha * delta_out' * Y1;
+        
+        change1 = change1(2:end,:);
+%         if(epoch > 1) 
+%             change1 = change1 + (beta .* (PREV_W1 - W1));
+%             change2 = change2 + (beta .* (PREV_W2 - W2));
+%         end
+        
+        W1 = W1+change1;
+        W2 = W2+change2;
+    end;
+    ES = E .* E;
+    if(epoch>1)
+        PREV_EES = EES;
+        EES = sum(ES(:))/2;
+        if(abs(PREV_EES - EES) < epsilon)
+            break;
+        end
+    else
+        EES = sum(ES(:))/2;
+    end
+    EES
+    epoch= epoch+1;
+end
